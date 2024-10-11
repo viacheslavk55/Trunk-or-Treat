1d7416334d0fd8795c4b24e3ea0673e06dc67492
viacheslavk55 <525vkostenko@frhsd.com>
2024-10-11 09:29:57 -0400

    finished the navbar

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    
+    $('.hidden-text').hide()
+    $('.menu-icon').on('click', function() {
+        $(this).toggleClass("active")
+        $('ul').toggleClass("active")
+    })
+    $('.hovers').on('click', function(){
+        $('.outer').hide()
+        $('.hidden-text').show()
+    })
 })
